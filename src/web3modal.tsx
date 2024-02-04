3f9dc404ff3ea76b12a0530619dc7e8c593167a2
Sergey Ukustov <sergey@ukstv.me>
2024-02-04 16:21:37 +0300

    Show how SIWE support is flawed

--- src/web3modal.tsx
+++ src/web3modal.tsx
@@ -1,10 +1,13 @@
 import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-
+import { createSIWEConfig } from "@web3modal/siwe";
 import { WagmiProvider } from "wagmi";
 import { arbitrum, mainnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { SIWECreateMessageArgs } from "@web3modal/core";
 import React from "react";
+import { getAccount } from "@wagmi/core";
+import { SiweMessage } from "siwe";
 
 // 0. Setup queryClient
 const queryClient = new QueryClient();
@@ -30,11 +33,54 @@ const config = defaultWagmiConfig({
   enableCoinbase: true, // Optional - true by default
 });
 
+const siweConfig = createSIWEConfig({
+  createMessage: ({ nonce, address, chainId }: SIWECreateMessageArgs) =>
+    new SiweMessage({
+      version: "1",
+      domain: window.location.host,
+      uri: window.location.origin,
+      address,
+      chainId,
+      nonce,
+      // Human-readable ASCII assertion that the user will sign, and it must not contain `\n`.
+      statement: "Sign in With Ethereum.",
+    }).prepareMessage(),
+  getNonce: async () => {
+    return Math.floor(Math.random() * 1_000_000_000).toString();
+  },
+  getSession: async () => {
+    const sessionString = localStorage.getItem("session-json");
+    if (!sessionString) {
+      console.error("Failed to get session");
+      throw new Error("Failed to get session");
+    }
+    const session = JSON.parse(sessionString);
+    if (!session) {
+      throw new Error("Failed to get session!");
+    }
+    const { address, chainId } = session;
+    return { address, chainId };
+  },
+  verifyMessage: async () => {
+    const { address, chainId } = getAccount(config);
+    if (address && chainId) {
+      localStorage.setItem(
+        "session-json",
+        JSON.stringify({ address: address, chainId: chainId }),
+      );
+    }
+    return true;
+  },
+  signOut: async () => {
+    return true;
+  },
+});
+
 // 3. Create modal
 createWeb3Modal({
   wagmiConfig: config,
   projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  siweConfig: siweConfig,
 });
 
 export function Web3Modal(props: React.PropsWithChildren) {
